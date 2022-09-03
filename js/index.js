902faf5bac0f7c8c27a7d5f9c9fcc652c2251206
invisible360 <zakirh817@gmail.com>
2022-09-03 14:24:33 +0600

    matha nosto hoye gese, commit korar kotha mone thake na

--- js/index.js
+++ js/index.js
@@ -28,16 +28,29 @@ const loadIndividualCategoryNews = async (id) => {
 
 const category = async () => {
     const dataRecv = await loadCategoryData();
+    console.log(dataRecv[0].category_id, dataRecv[0].category_name);
     const categoryContainer = document.getElementById('category-container');
+
     dataRecv.forEach(category => {
         const { category_name, category_id } = category // destructuring
         const li = document.createElement('li');
-        li.setAttribute(`onclick`, `clickedCategory('${category_id}', '${category_name}')`)
+        li.setAttribute(`onclick`, `clickedCategory('${category_id}', '${category_name}')`);
+        li.setAttribute(`id`, `news-${category_id}`);
         li.innerHTML = `
         <a>${category_name}</a>
         `
         categoryContainer.appendChild(li);
     });
+
+    document.getElementById('news-' + dataRecv[0].category_id).classList.add('font-semibold');
+    const defaultId = await loadIndividualCategoryNews(dataRecv[0].category_id);
+    findingMsg(defaultId.length, dataRecv[0].category_name);
+
+    defaultId.forEach(element => {
+
+    });
+
+
 }
 
 const findingMsg = (len, catName) => {
@@ -55,25 +68,29 @@ const clickedCategory = async (id, catName) => {
     const newsLength = individualCategory.length;
     findingMsg(newsLength, catName);
 
+    // ekhane............
+
     // console.log(individualCategory);
 
     const newsContainer = document.getElementById('news-container');
-    newsContainer.innerHTML = ``
+    newsContainer.innerHTML = ``;
 
     individualCategory.forEach(infoToDisplay => {
         // console.log(element);
         const { title, details, thumbnail_url, total_view, author } = infoToDisplay;
-        const { name:authorName, published_date, img } = author;
-        console.log(total_view);
+        const { name: authorName, published_date, img } = author;
+        // console.log(total_view);
 
-        const div = document.createElement ('div');
+        // const div = dynamiCard(title, details, thumbnail_url, total_view, author, authorName, published_date, img);
+
+        const div = document.createElement('div');
         div.innerHTML = `
         <div class="card lg:card-side bg-base-100 drop-shadow-2xl my-5">
         <figure><img class="p-5 object-contain h-full w-96"
                 src=${thumbnail_url} alt="Album"></figure>
         <div class="card-body">
             <h2 class="card-title">${title}</h2>
-            <p>${details.slice (0, 200)+'...'}</p>
+            <p>${details.slice(0, 200) + '...'}</p>
 
             <div class="flex-none lg:flex items-center justify-between mt-5 lg:mt-0">
                 <div class="flex lg:flex-none items-center justify-between">
@@ -82,11 +99,11 @@ const clickedCategory = async (id, catName) => {
                             <div class="avatar">
                                 <div class="w-14 rounded-full">
                                     <img
-                                        src='${authorName === 'system' || authorName === '' || authorName === null ? 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/450px-No_image_available.svg.png': img}' />
+                                        src='${authorName === 'system' || authorName === '' || authorName === null ? 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/450px-No_image_available.svg.png' : img}' />
                                 </div>
                             </div>
                             <div class="flex flex-col mx-3">
-                                <p class="text-sm lg:text-base">${authorName === 'system' || authorName === '' || authorName === null ? 'Not Available': authorName}</p>
+                                <p class="text-sm lg:text-base">${authorName === 'system' || authorName === '' || authorName === null ? 'Not Available' : authorName}</p>
                                 <p class="text-xs text-slate-500">${published_date}</p>
                             </div>
                         </div>
@@ -94,7 +111,7 @@ const clickedCategory = async (id, catName) => {
 
                     <div class="flex items-center ml-0 lg:ml-10">
                         <i class="fa-regular fa-eye"></i>
-                        <p class="mx-1 font-bold">${total_view === null ? 'Unknown': total_view}</p>
+                        <p class="mx-1 font-bold">${total_view === null ? 'Not Available' : total_view}</p>
                     </div>
                 </div>
 
@@ -116,11 +133,21 @@ const clickedCategory = async (id, catName) => {
         </div>
     </div>
         `
-
-        newsContainer.appendChild (div)
+        newsContainer.appendChild(div)
     });
+
+    if (newsLength === 0) {
+        document.getElementById('footer').classList.add('fixed', 'bottom-0', 'left-0', 'right-0');
+    }
+    else {
+        document.getElementById('footer').classList.remove('fixed', 'bottom-0', 'left-0', 'right-0');
+
+    }
+
+
 
 
 }
+
 
 category();
